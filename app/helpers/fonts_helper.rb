d6ad98b1918c1c83b39e139f45657660e0985bb4
Minh Phan <pbminh2511@gmail.com>
2020-03-28 09:55:10 +0700

    Modify draw_text to account for new line

--- app/helpers/fonts_helper.rb
+++ app/helpers/fonts_helper.rb
@@ -1,16 +1,61 @@
 module FontsHelper
     def draw_text(font = nil, text = "", width = 100, height = 100, fn = 'font.png')
         canvas = Magick::Image.new(width,height) { self.background_color = "Transparent" }
-        text.upcase!
+        
+        text = text.upcase.strip
+
         anno = Magick::Draw.new.tap do |a|
-            a.gravity = Magick::CenterGravity
+            a.gravity = Magick::NorthWestGravity
             a.font = font if font
-            a.pointsize = 20
+            a.pointsize = 15
             a.font_weight = 800
+            a.fill = "red"
         end
+
+        text = add_new_line(text,anno,canvas)
 
         anno.annotate(canvas, 0, 0, 0, 0, text)
 
         canvas.write(fn)
+    end
+
+    def add_new_line(text, anno, canvas, start_width = 0)
+        chars = text.split("")
+        sum_width = start_width
+        new_line_pos = []
+        last_word_pos = 0
+
+        chars.each_with_index do |char,i|
+            sum_width += width_of_char(char, anno, canvas, i)
+
+            if sum_width >= canvas.columns
+                new_line_pos << i
+                sum_width = 0
+            end
+        end
+        
+        new_line_pos.each do |pos|
+            chars.insert(pos, "\n")
+        end
+    
+        return chars.join('')
+    end
+    
+    def width_of_char(char, anno, canvas, index)
+        anno_dup = anno.dup
+        canvas_dup = canvas.dup
+        fixed_width = 0
+        if index != -1
+            char = char + 'A'
+            fixed_width = width_of_char('A', anno, canvas, -1)
+        end
+        anno_dup.annotate(canvas_dup, 0, 0, 0, 0, char)
+        canvas_dup.write("#{Rails.root}/rmagick_temp/temp#{index}.png")
+        
+        #last_word_pos = i + 1 if char = ' '
+        
+        metrics = anno_dup.get_type_metrics(canvas_dup, char)
+        
+        return metrics.width - fixed_width
     end
 end
--- test.rb
+++ test.rb
@@ -1,10 +1,58 @@
-require 'RMagick'
-text = "test!"
-canvas = Magick::Image.new(100,100) { self.background_color = 'red' }
-anno = Magick::Draw.new.tap do |t|
-    t.gravity = Magick::CenterGravity
+require 'rmagick'
+def draw_text(font = nil, text = "", width = 100, height = 100, fn = 'font.png')
+    canvas = Magick::Image.new(width,height) { self.background_color = "Blue" }
+    text = text.upcase.strip
+    anno = Magick::Draw.new.tap do |a|
+        a.gravity = Magick::NorthWestGravity
+        a.font = font if font
+        a.pointsize = 15
+        a.font_weight = 800
+        a.fill = "red"
+        a.gravity = Magick::CenterGravity
+    end
+    text = add_new_line(text, anno, canvas)
+    anno.annotate(canvas, 0, 0, 0, 0, text)
+
+    canvas.display
+end
+def add_new_line(string, anno, canvas, start_width = 0)
+    arr = string.split("")
+    sumw = start_width
+    nlpos = []
+    last_word_pos = 0
+    lw = 0
+    arr.each_with_index do |ele,i|
+        dup = anno.dup
+        dcanvas = canvas.dup
+        dcanvs2 = canvas.dup
+        ele = ele + arr[0] if i!=0
+        dup.annotate(dcanvas, 0, 0, 0, 0, ele)
+        dup.annotate(dcanvs2, 0, 0, 0, 0, ele[0])
+        dcanvas.write("rmagick_temp/temp#{i}.png")
+        metrics = dup.get_type_metrics(dcanvas, ele)
+        dcv2 = dup.get_type_metrics(dcanvs2,ele[0]).width
+        if ele == ' '
+            last_word_pos = i + 1
+        end
+        puts ("#{sumw} #{metrics.width} #{metrics.width - lw - dcv2} #{dcanvas.columns}")
+        sumw += metrics.width - lw
+        if i == 0
+            lw = metrics.width
+        end
+        if sumw >= dcanvas.columns
+            nlpos << i
+            sumw = 0
+        end
+    end
+    
+    nlpos.each do |ele|
+        arr.insert(ele, "\n")
+        p ele
+    end
+
+    p arr
+    return arr.join('')
 end
 
-anno.annotate(canvas, 0, 0, 0, 0, text)
-
-p canvas.to_blob
+draw_text("","ABCDEFGHJKLOPQRSTUVWXYZMN123456789")
+exit
