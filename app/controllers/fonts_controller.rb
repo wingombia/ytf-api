6f734a88ddbff2bc2b421e46338737a66c666362
Minh Phan <pbminh2511@gmail.com>
2020-03-29 15:38:05 +0700

    Refactor code and add option hash to draw_text function

--- app/controllers/fonts_controller.rb
+++ app/controllers/fonts_controller.rb
@@ -1,19 +1,21 @@
 class FontsController < ApplicationController
     before_action :unlock_cors
     def get
-        if !(params[:text] && params[:font])
-            return render json: { errors: "no text and/or font params" }, status: :unprocessable_entity
-        end
-
-        draw_text(params[:font], params[:text], params[:size])
-        send_file "#{Rails.root}/font.png", type: "image/png"#, disposition: 'inline'
-        #render json: {url: "#{Rails.root}/font#{params[:size]}.png"}
+      draw_text(option_params)
+      send_file "#{Rails.root}/font.png", type: "image/png"#, disposition: 'inline'
     end
+
     private 
-     def unlock_cors
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-        headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-     end
+     
+    def option_params
+      params.permit(:font, :text, :size, :color, :weight,
+                    :bg_color, :bg_width, :bg_height)
+    end
+
+    def unlock_cors
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
 end
--- app/helpers/fonts_helper.rb
+++ app/helpers/fonts_helper.rb
@@ -1,73 +1,102 @@
 module FontsHelper
-    MAX_SIZE = 50
-    def draw_text(font = nil, text = "", size = 12, width = 500, height = 500, fn = 'font.png')
-        canvas = Magick::Image.new(width,height) { self.background_color = "Transparent" }
-        
-        text = text.upcase.strip
+  MAX_LETTER_HEIGHT, MAX_FONT_SIZE = 50, 50
+  MAX_LETTER_WIDTH = 30
+  FILE_NAME = "font.png"
 
-        anno = Magick::Draw.new.tap do |a|
-            a.gravity = Magick::NorthWestGravity
-            a.font = font if font
-            a.pointsize = size.to_f
-            a.font_weight = 800
-            a.fill = "red"
-        end
-        text = add_new_line(text, anno, canvas, 0)
-
-        anno.annotate(canvas, 0, 0, 0, 0, text)
-
-        canvas.write(fn)
-    end
-
-    def add_new_line(text, anno, canvas, start_width = 0)
-        chars = text.split("")
-        sum_width = start_width
-        new_line_pos = []
-        last_word_pos = 0
-        width_since_last_space = 0
-        space_width = width_of_ws(anno)
-        padding_right = 10
-        word = ''
+  def draw_text(options)
+    options = get_default_value(options)
+    canvas = get_canvas(options)
+    anno = get_annotation(options)
     
-        chars.each_with_index do |char,i|
-            if char != ' '
-                word = word + char
-                next if i < chars.length - 1
-            end
+    text = options[:text].strip
+    text = add_new_line(text,anno,canvas)
 
-            woc = width_of_char(word, anno)
-            sum_width += woc 
-            sum_width += space_width if i < chars.length - 1
+    anno.annotate(canvas, 0, 0, 0, 0, text)
+    canvas.write(FILE_NAME)
+  end
 
-            word = ''
+  private
 
-            if sum_width >= canvas.columns
-                new_line_pos << last_word_pos
-                sum_width = woc
-            end
+  def get_canvas(options)
+    canvas = Magick::Image.new(options[:bg_width], options[:bg_height]) do
+      self.background_color = options[:bg_color]
+    end
 
-            last_word_pos = i if char == ' '
-        end
-        
-        new_line_pos.each do |pos|
-            chars[pos] = "\n"
-        end
-        
-        return chars.join('')
+    canvas
+  end
+
+  def get_annotation(options)
+    anno = Magick::Draw.new.tap do |a|
+      a.gravity = Magick::NorthWestGravity
+      a.font = options[:font]
+      a.pointsize = options[:size].to_f
+      a.font_weight = options[:weight]
+      a.fill = options[:color]
+    end 
+    anno
+  end
+
+  def get_default_value(options)
+    options[:font]      ||= "Helvetica"
+    options[:size]      ||= 12
+    options[:weight]    ||= 400
+    options[:color]     ||= "red"
+    options[:bg_color]  ||= "Transparent"
+    options[:bg_width]  ||= 500
+    options[:bg_height] ||= 500
+    options[:text]      ||= "Default Text"
+    
+    options
+  end
+
+  def add_new_line(text, anno, canvas, start_width = 0)
+    chars = text.split("")
+    sum_width = start_width
+    new_line_pos = []
+    last_word_pos = 0
+    width_since_last_space = 0
+    space_width = width_of_ws(anno)
+    padding_right = 10
+    word = ''
+
+    chars.each_with_index do |char,i|
+      if char != ' '
+        word = word + char
+        next if i < chars.length - 1
+      end
+
+      woc = width_of_char(word, anno)
+      sum_width += woc 
+      sum_width += space_width if i < chars.length - 1
+      word = ''
+
+      if sum_width >= canvas.columns
+          new_line_pos << last_word_pos
+          sum_width = woc
+      end
+
+      last_word_pos = i if char == ' '
     end
     
-    def width_of_char(char, anno, index = 0)
-        
-        anno_dup = anno.dup
-        canvas_dup = Magick::Image.new(MAX_SIZE * char.length, 30) { self.background_color = "Transparent" }
-
-        anno_dup.annotate(canvas_dup, 0, 0, 0, 0, char)
-        metrics = anno_dup.get_type_metrics(canvas_dup, char)
+    new_line_pos.each do |pos|
+      chars[pos] = "\n"
+    end
     
-        return metrics.width
-    end
+    chars.join('')
+  end
 
-    def width_of_ws(anno)
-        return width_of_char("B A", anno) - width_of_char("A", anno) - width_of_char("B", anno)
-    end
+  def width_of_char(chars, anno, index = 0)
+    
+    anno_dup = anno.dup
+    canvas_dup = Magick::Image.new(MAX_LETTER_HEIGHT * chars.length, MAX_LETTER_WIDTH) { self.background_color = "Transparent" }
+
+    anno_dup.annotate(canvas_dup, 0, 0, 0, 0, chars)
+    metrics = anno_dup.get_type_metrics(canvas_dup, chars)
+
+    metrics.width
+  end
+
+  def width_of_ws(anno)
+    width_of_char("B A", anno) - width_of_char("A", anno) - width_of_char("B", anno)
+  end
 end
